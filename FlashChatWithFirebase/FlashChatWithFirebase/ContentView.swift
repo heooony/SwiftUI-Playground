18edd8a0fdbf42a98b44539c204d73368c177060
oueya1479 <oueya1479@naver.com>
2022-07-20 21:55:15 +0900

    add firebase app

---
+++ FlashChatWithFirebase/FlashChatWithFirebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlashChatWithFirebase
+//
+//  Created by 김동헌 on 2022/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlashChatWithFirebase/FlashChatWithFirebase/FlashChatWithFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashChatWithFirebaseApp.swift
+//  FlashChatWithFirebase
+//
+//  Created by 김동헌 on 2022/07/20.
+//
+
+import SwiftUI
+
+@main
+struct FlashChatWithFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
