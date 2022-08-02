a550065a9d3510572ce0a52a575d8e8c358b3f53
oueya1479 <oueya1479@naver.com>
2022-08-02 20:59:01 +0900

    add hexcolor

---
+++ InstagramSwiftUI/InstagramSwiftUI/Utils/HexColor.swift
@@ -0,0 +1,20 @@
+//
+//  HexColor.swift
+//  NilGround
+//
+//  Created by 김동헌 on 2022/07/04.
+//
+
+import SwiftUI
+
+extension Color {
+  init(_ hex: UInt, alpha: Double = 1) {
+    self.init(
+      .sRGB,
+      red: Double((hex >> 16) & 0xFF) / 255,
+      green: Double((hex >> 8) & 0xFF) / 255,
+      blue: Double(hex & 0xFF) / 255,
+      opacity: alpha
+    )
+  }
+}
